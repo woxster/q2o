0261cedff919b850a03242151de5f326ffd9c282
h-thurow <thurow.h@googlemail.com>
2019-03-13 07:31:34 +0100

    Switch q2o's configuration without deinitializing it first.

--- src/main/java/com/zaxxer/q2o/q2o.java
+++ src/main/java/com/zaxxer/q2o/q2o.java
@@ -19,6 +19,7 @@ public final class q2o {
     * @return dataSource that will be used for queries
     */
    public static DataSource initializeTxNone(DataSource dataSource) {
+      deinitialize();
       SqlClosure.setDefaultDataSource(dataSource);
       Q2Obj.q2Object = new Q2Object();
       return dataSource;
@@ -31,9 +32,14 @@ public final class q2o {
     * @return dataSource that will be used for queries
     */
    public static DataSource initializeTxSimple(DataSource dataSource) {
+      deinitialize();
       Q2Obj.q2Object = new Q2Object();
       TxTransactionManager txManager = new TxTransactionManager(dataSource);
-      return initializeTxCustom(txManager.getTxDataSource(), txManager, txManager);
+      TransactionHelper.setTransactionManager(txManager);
+      TransactionHelper.setUserTransaction(txManager);
+      DataSource txDataSource = txManager.getTxDataSource();
+      SqlClosure.setDefaultDataSource(txDataSource);
+      return txDataSource;
    }
 
    /**
@@ -45,16 +51,19 @@ public final class q2o {
     * @return dataSource that will be used for queries
     */
    public static DataSource initializeTxCustom(DataSource dataSource, TransactionManager txManager, UserTransaction userTx) {
+      deinitialize();
       Q2Obj.q2Object = new Q2Object();
       TransactionHelper.setTransactionManager(txManager);
       TransactionHelper.setUserTransaction(userTx);
-      return initializeTxNone(dataSource);
+      SqlClosure.setDefaultDataSource(dataSource);
+      return dataSource;
    }
 
    /**
     * To make q2o support spring managed transactions, if available.
     */
    public static DataSource initializeWithSpringTxSupport(DataSource dataSource) {
+      deinitialize();
       Q2Obj.q2Object = new Q2Object();
       Q2Sql.isSpringTxAware = true;
       Q2ObjList.isSpringTxAware = true;
@@ -65,16 +74,15 @@ public final class q2o {
    }
 
    /**
-    * You can reset q2o to a fresh state if desired.
-    * E.g. if you want to call another initializeXXX method.
+    * To explicitly reset q2o to a fresh state if desired. E.g. if you want to call another initializeXXX method. This call is optional because all initializeXXX methods will call deinitialize() anyway.
     */
    public static void deinitialize() {
-      SqlClosure.setDefaultDataSource(null);
       TransactionHelper.setUserTransaction(null);
       TransactionHelper.setTransactionManager(null);
       Q2Obj.q2Object = null;
       Q2Sql.isSpringTxAware = false;
       Q2ObjList.isSpringTxAware = false;
+      SqlClosure.setDefaultDataSource(null);
       SqlClosure.isSpringTxAware = false;
       SqlClosure.setDefaultExceptionTranslator(null);
    }
